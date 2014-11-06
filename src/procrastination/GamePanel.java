1bf63b0195f54d9cbefe5682a72afd88ad9c65ea
imburnis <jkradatz@gmail.com>
2014-11-05 20:00:26 -0500

    Created the window as well as the keyboard input because it kind of sucks to have a full screen window that you cannot close unless you click the windows key and right click on the program.  - The main class extends a JFrame and creates the window as well as sets it to be full screen  - Then it creates a GamePanel which contains all of the run logic for the game.  - There is keyPressed and keyReleased events that take the input from the keyboard. However, these happen asynchronously so we will have to implement a system for keeping track of controls
    **This game loop is frame rate based over variable time step. Right now it is set to thirty frames per second because we probably wont need any higher than that.

---
+++ src/procrastination/GamePanel.java
@@ -0,0 +1,261 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package procrastination;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author jradatz
+ */
+public class GamePanel extends JPanel implements Runnable{
+    private JFrame container;
+    
+    public Dimension gameWindowSize;    //Pixel size of the buffer
+    private Dimension screenSize;       //Size of the actual screen
+    private Rectangle drawScale;        //Calculated scale and position of the on screen viewport
+    
+    //Buffer Components
+    private Image dbImage;  //Buffer where everything is drawn before being flipped to the panel
+    private Graphics dbg;   //Stores the buffers graphics for easy transport
+    
+    //FPS Variables
+    private final double DESIRED_FPS = 30;
+    private final double DESIRED_FRAMERATE = (1 / DESIRED_FPS) * 1000;
+    private final double MAX_BEFORE_SYNC = 2 * DESIRED_FRAMERATE;
+    private final int MAX_FRAMESKIP = 5;
+    private int updateSecondStart; //The start if this second for updates
+    private int updatesPerSecond; //The number of updates for this second
+    private int updateCycles; //The last updates per second that was calculated
+    private int drawsPerSecond; //The number of draws for this second
+    private int drawCycles; //The last calculated draws per second
+    
+    private Thread gameThread;
+    private boolean running = false;
+    
+    /**
+     * Takes the game window size as well as the containing JPanel
+     * @param wPort - The width of the draw buffer
+     * @param hPort - The height of the draw buffer
+     * @param owner - The JFrame that this panel is contained within
+     */
+    GamePanel(int wPort, int hPort, JFrame owner){
+        gameWindowSize = new Dimension(wPort, hPort);
+        container = owner;
+        setBackground(Color.BLACK);
+    }
+    
+    /**
+     * Creates a new thread of the game loop logic
+     */
+    public void startGame(){
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    /**
+     * This is where all of the update logic of the game will take place
+     */
+    private void gameUpdate(){
+        
+    }
+    /**
+     * Called by the game loop. This is where all of our draw code will go
+     * @param g - The graphics object that will be used for drawing
+     */
+    private void gameDraw(Graphics g){
+        g.setColor(Color.white);
+        g.drawString("UpdatesPerSecond: " + updatesPerSecond + "; DrawsPerSecond: " + drawsPerSecond, 10, 10);
+        g.setColor(Color.red);
+        g.drawRect(0, 0, gameWindowSize.width - 1, gameWindowSize.height - 1);
+    }
+    
+    /**
+     * Called by the JFrame's KeyAdapter
+     * @param keyCode - the keycode of the key that the event was caused by
+     */
+    public void key_pressed_event(int keyCode) {
+        switch(keyCode){
+            case KeyEvent.VK_ESCAPE:
+                endGame();
+                break;
+        }
+    }
+
+    /**
+     * Called by the JFrame's KeyAdapter
+     * @param keyCode - the keycode of the key that the event was caused by
+     */
+    public void key_released_event(int keyCode) {
+        
+    }
+    
+    /**
+     * This function will end the game
+     */
+    public void endGame(){
+        running = false;
+    }
+    
+    /**
+     * Not completely necessary to unload resources in java because its managed
+     * However, this function is always called at the end of the game loop
+     */
+    public void unload(){
+        
+    }
+    
+    /**
+     * Called whenever the user closes the game window through explorer rather than our exit features
+     */
+    public void window_closing(){
+        running = false;
+    }
+    
+    /**
+     * The main logic that controls when things can run and sleeps while waiting for the next frame
+     */
+    public void run(){
+        //Starts FPS counter
+        System.out.println("System Starting");
+        updateSecondStart = (int) (System.nanoTime() / 1000000);
+        updateCycles = 0;
+        updatesPerSecond = 0;
+        drawCycles = 0;
+        drawsPerSecond = 0;
+
+        //next_update - the next time the loop should cycle
+        double next_update = (double) System.nanoTime() / 1000000;
+        int skipped_frames = 0;
+
+        while (running) {
+            double time = (double) System.nanoTime() / 1000000;
+            //Update the updatesPerSecond counters
+            if ((time - updateSecondStart) >= 1000) {
+                updatesPerSecond = updateCycles;
+                updateCycles = 0;
+                drawsPerSecond = drawCycles;
+                drawCycles = 0;
+                updateSecondStart = (int)time;
+            }
+            //If the game loop is significantly behind it stops trying to catch
+            //up and resyncs itself to current time
+            if ((time - next_update) > MAX_BEFORE_SYNC) {
+                next_update = time;
+            }
+            //If it is time to run an update cycle
+            if (time >= next_update) {
+                next_update += DESIRED_FRAMERATE;
+                updateCycles++;
+                gameUpdate();
+                //If there is time to update or there has been a
+                //large number of frame skips it renders the screen
+                if (time < next_update || skipped_frames >= MAX_FRAMESKIP) {
+                    gameRender();
+                    paintScreen();
+                    skipped_frames = 0;
+                    drawCycles++;
+                } else {
+                    skipped_frames++;
+                }
+            }else {
+                //Tells the thread to sleep when its bored
+                int sleep = (int) (next_update - time);
+                if (sleep > 0) {
+                    try {
+                        gameThread.sleep(sleep);
+                    } catch (InterruptedException ex) {
+                        //Ignore
+                    }
+                }
+            }
+        }
+        //Clean up stuffs
+        unload();
+        container.dispose();
+        System.out.println("System Exiting");
+    }
+    
+    /**
+     * Calculates the scale from the gameScreen size to the screen size as well as centers
+     * @param d - The dimension of the screen (usually straight from JFrame method getSize())
+     */
+    private void setWindowSize(Dimension d){
+        screenSize = d;
+        drawScale = new Rectangle();
+        if(screenSize.width / gameWindowSize.width < screenSize.height / gameWindowSize.height){
+            //wider than tall compared to screen
+            drawScale.width = screenSize.width;
+            drawScale.x = 0;
+            drawScale.height = (int)(((double)screenSize.width / (double)gameWindowSize.width) * (double)gameWindowSize.height);
+            drawScale.y = (screenSize.height - drawScale.height) / 2;
+        }else{
+            //taller than wide compared to screen
+            drawScale.width = (int)(((double)screenSize.height / (double)gameWindowSize.height) * (double)gameWindowSize.width);
+            drawScale.x = (screenSize.width - drawScale.width) / 2;
+            drawScale.height = screenSize.height;
+            drawScale.y = 0;
+        }
+        setSize(screenSize);
+    }
+    
+    /**
+     * Used for rendering all of the game to the buffer
+     */
+    private void gameRender(){
+        //Creates Double Buffer with appropriate error checking
+        if (dbImage == null) {
+            try{
+                dbImage = createImage(gameWindowSize.width, gameWindowSize.height);
+            }catch(NullPointerException ex){
+                System.err.println(ex);
+            }
+            if (dbImage == null) {
+                System.out.println("Failed to Init DB");
+                return;
+            }
+            dbg = dbImage.getGraphics();
+        }
+        //Flush Double Buffer
+        dbg.setColor(Color.BLACK);
+        dbg.fillRect(0, 0, gameWindowSize.width, gameWindowSize.height);
+        //Draw Game Elements
+        dbg.setColor(Color.BLACK);
+        //HEY DRAW HERE
+        gameDraw(dbg);
+    }
+    
+    /**
+     * Flips the buffer to the panel
+     */
+    private void paintScreen(){
+        Graphics g;
+        try {
+            g = this.getGraphics();
+            if (g != null && dbImage != null) {
+                if(screenSize == null || screenSize.width == 0 || screenSize.height == 0){
+                    setWindowSize(container.getSize());
+                }
+                g.drawImage(dbImage, drawScale.x, drawScale.y, drawScale.width, drawScale.height, null);
+            }else{
+                System.out.println("Error getting graphics");
+            }
+            Toolkit.getDefaultToolkit().sync();//Necesary for certain operating systems
+            g.dispose();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+}
--- src/procrastination/Procrastination.java
+++ src/procrastination/Procrastination.java
@@ -1,7 +1,114 @@
 package procrastination;
 
-public class Procrastination {
-	public static void main(String[] args) {
-		
-	}
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Procrastination extends JFrame implements Runnable{
+	
+    private static JFrame owner;
+    private static WindowListener wl;
+    private static KeyListener kl;
+    private static GamePanel content;
+    
+    //The width and height of the draw region in pixels
+    private int hPort = 600;
+    private int wPort = 800;
+    
+    public static void main(String[] args) {
+        owner = new Procrastination();
+    }
+        
+    Procrastination(){
+        SwingUtilities.invokeLater(this);
+    }
+
+    @Override
+    public void run() {
+        //Changes some JFrsame settings for full screen
+        getContentPane().setBackground(Color.black);
+        setFocusTraversalKeysEnabled(false);
+        setUndecorated(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //Gets the GraphicsDevice to set the window as a full screen window
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = environment.getDefaultScreenDevice();
+        
+        device.setFullScreenWindow(this);
+        
+        //Create and add the GamePanel
+        content = new GamePanel(wPort, hPort, this);
+        
+        //Make the window not go away on close so that we can do gracefull closing
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //Create a window listener that we can add to the JFrame to generate an event when the window
+        //is sent a close event from windows (like when the user presses the close button
+        wl = new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+            }
+            
+            @Override
+            public void windowClosing(WindowEvent e) {
+                content.window_closing();
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+            }
+        };
+        addWindowListener(wl);
+        
+        //Allow for keyboard input to be passed from the JFrame to the GamePanel
+        kl = new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    content.key_pressed_event(e.getKeyCode());
+                    e.consume();
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    content.key_released_event(e.getKeyCode());
+                    e.consume();
+                }
+
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    e.consume();
+                }
+            };
+            addKeyListener(kl);
+        
+        //Add the GamePanel to the window and show it
+        add(content);
+        setVisible(true);
+        //Start the game loop
+        content.startGame();
+    }
 }
