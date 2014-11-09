cf8d16afda5430ed3cde0d327e824134e0488309
kjarosz <kjarosz19946@gmail.com>
2014-11-09 18:06:32 -0500

    Add enemy sprites.

--- src/procrastination/content/Enemy.java
+++ src/procrastination/content/Enemy.java
@@ -1,37 +1,67 @@
 package procrastination.content;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Enemy {
+   private final Rectangle SPRITES[] = {
+         new Rectangle(28, 34, 191, 362),
+         new Rectangle(213, 12, 377, 362)
+   };
+   
    private final double TERMINAL_VELOCITY = 90; // pixels / second
    
    private Point2D.Double mPosition;
-   private Point2D.Double mVelocity;
+   private Point2D.Double mDirection;
+   
+   private BufferedImage mSprites[];
+   private int mCurrentImage;
    
    private long mLastTime; // milliseconds
    
    public Enemy(Point2D.Double position) {
       mPosition = position;
-      mVelocity = new Point2D.Double();
+      mDirection = new Point2D.Double();
       
       mLastTime = System.currentTimeMillis();
+      
+      loadSprites();
    }
    
-   private void setVelocity(Point2D.Double direction) {
-      double length = Math.sqrt(direction.x*direction.x + direction.y*direction.y);
-      mVelocity.x = (direction.x / length) * TERMINAL_VELOCITY;
-      mVelocity.y = (direction.y / length) * TERMINAL_VELOCITY;
+   private void loadSprites() {
+      try {
+         BufferedImage spriteSheet = ImageIO.read(new File("images" + File.separator + "enemy-walk-sheet.png"));
+         mSprites = new BufferedImage[SPRITES.length];
+         for(int i = 0; i < SPRITES.length; i++) {
+            mSprites[i] = spriteSheet.getSubimage(
+                  SPRITES[i].x, 
+                  SPRITES[i].y, 
+                  SPRITES[i].width - SPRITES[i].x, 
+                  SPRITES[i].height - SPRITES[i].y);
+         }
+      } catch(IOException ex) {
+         
+      }
+   }
+   
+   private void setDirection(Point2D.Double playerPosition) {
+      mDirection.x = playerPosition.x - mPosition.x;
+      mDirection.y = playerPosition.y - mPosition.y;
+      double dirLength = Math.sqrt(mDirection.x*mDirection.x + mDirection.y*mDirection.y);
+      mDirection.x /= dirLength;
+      mDirection.y /= dirLength;
    }
    
    public void update(Player player) {
-      Point2D.Double playerPosition = player.getPosition();
-      Point2D.Double directionVector = new Point2D.Double();
-      directionVector.x = playerPosition.x - mPosition.x;
-      directionVector.y = playerPosition.y - mPosition.y;
-      setVelocity(directionVector);
+      setDirection(player.getPosition());
       
       move();
    }
@@ -40,14 +70,28 @@ public class Enemy {
       long deltaTime = System.currentTimeMillis() - mLastTime;
       mLastTime += deltaTime;
       
-      mPosition.x += mVelocity.x * deltaTime / 1000.0f;
-      mPosition.y += mVelocity.y * deltaTime / 1000.0f;
+      mPosition.x += mDirection.x * TERMINAL_VELOCITY * deltaTime / 1000.0f;
+      mPosition.y += mDirection.y * TERMINAL_VELOCITY * deltaTime / 1000.0f;
    }
    
    public void draw(Graphics g) {
-      Color oldColor = g.getColor();
-      g.setColor(Color.BLUE);
-      g.fillRect((int)mPosition.x - 16, (int)mPosition.y - 16, 32, 32);
-      g.setColor(oldColor);
+      BufferedImage image = mSprites[mCurrentImage];
+      
+      Graphics2D g2 = (Graphics2D)g;
+      AffineTransform oldTransform = g2.getTransform();
+      g2.translate(mPosition.x, mPosition.y);
+      if(mDirection.x < 0) {
+         g2.rotate(Math.atan(mDirection.y / mDirection.x) - 3.14/2);
+      } else {
+         g2.rotate(Math.atan(mDirection.y / mDirection.x) + 3.14/2);
+      }
+      g2.scale(0.5, 0.5);
+      g2.drawImage(image, 
+            image.getWidth()/2, 
+            image.getHeight()/2,
+            -image.getWidth(),
+            -image.getHeight(),
+            null);
+      g2.setTransform(oldTransform);
    }
 }
