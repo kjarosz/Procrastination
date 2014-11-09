7955ebe7fed505f35bd4392288f1ebd69336478c
kjarosz <kjarosz19946@gmail.com>
2014-11-09 16:25:11 -0500

    Add enemy class.

---
+++ src/procrastination/content/Enemy.java
@@ -0,0 +1,51 @@
+package procrastination.content;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Point2D;
+
+public class Enemy {
+   private final double TERMINAL_VELOCITY = 90; // pixels / second
+   
+   private Point2D.Double mPosition;
+   private Point2D.Double mVelocity;
+   
+   private long mLastTime; // milliseconds
+   
+   public Enemy(Point2D.Double position) {
+      mPosition = position;
+      
+      mLastTime = System.currentTimeMillis();
+   }
+   
+   private void setVelocity(Point2D.Double direction) {
+      double length = Math.sqrt(direction.x*direction.x + direction.y*direction.y);
+      mVelocity.x = (direction.x / length) * TERMINAL_VELOCITY;
+      mVelocity.y = (direction.y / length) * TERMINAL_VELOCITY;
+   }
+   
+   public void update(Player player) {
+      Point2D.Double playerPosition = player.getPosition();
+      Point2D.Double directionVector = new Point2D.Double();
+      directionVector.x = playerPosition.x - mPosition.x;
+      directionVector.y = playerPosition.y - mPosition.y;
+      setVelocity(directionVector);
+      
+      move();
+   }
+   
+   private void move() {
+      long deltaTime = System.currentTimeMillis() - mLastTime;
+      mLastTime += deltaTime;
+      
+      mPosition.x += mVelocity.x * deltaTime / 1000.0f;
+      mPosition.y += mVelocity.y * deltaTime / 1000.0f;
+   }
+   
+   public void draw(Graphics g) {
+      Color oldColor = g.getColor();
+      g.setColor(Color.BLUE);
+      g.fillRect((int)mPosition.x - 16, (int)mPosition.y - 16, 32, 32);
+      g.setColor(oldColor);
+   }
+}
--- src/procrastination/content/Level.java
+++ src/procrastination/content/Level.java
@@ -2,16 +2,19 @@ package procrastination.content;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.LinkedList;
 
 public class Level {
    private Dimension mLevelSize;
    
    private Player mPlayer;
+   private LinkedList<Enemy> mEnemies;
    
 	public Level(int width, int height) {
 	   mLevelSize = new Dimension(width, height);
 		loadLevel();
       mPlayer = new Player(mLevelSize.width, mLevelSize.height);
+      mEnemies = new LinkedList<>();
 	}
 
 	private void loadLevel() {
@@ -20,9 +23,22 @@ public class Level {
 	
 	public void update() {
 	   mPlayer.update();
+	   
+	   spawnNewEnemies();
+	   
+	   for(Enemy enemy: mEnemies) {
+	      enemy.update(mPlayer);
+	   }
+	}
+	
+	private void spawnNewEnemies() {
+	   
 	}
 	
 	public void draw(Graphics g) {
 	   mPlayer.draw(g);
+	   
+	   for(Enemy enemy: mEnemies)
+	      enemy.draw(g);
 	}
 }
